56efdf9ea1fe575fc5e0b20e3684035919be1311
AutoSys Application User Prod <ldautop@akrsrv315.na.goodyear.com>
2023-01-26 02:57:51 -0500

    fixed bug for longrunning job script where jobs are appearing randomly appearing every script run

--- shell/longrunning00.py
+++ shell/longrunning00.py
@@ -13,14 +13,17 @@ on_ice_jobs = " OI "
 oh_job_count = 0
 oi_job_count = 0
 ru_job_count = 0
+save_path = '/var/autosys/prd/int/working/'
+file_name = "runjob_output.tsv"
+CompleteFilePath = os.path.join(save_path, file_name)
 
 #open the file jobstat.txt as readable
-with open("/opt/cascripts/shell/longrun_a.dat","r") as jobstat:
+with open("/var/autosys/prd/int/working/longrun_a.dat","r") as jobstat:
 
 #create an output file runjob_output.txt
-    with open("runjob_output.tsv", "w") as output_file:
+    with open(CompleteFilePath, "w") as output_file:
         #create headers for each columns
-        output_file.write("Job Name                                                         Start Date           Duration\n")
+        output_file.write("Job Name                                                         Start Date Time      Duration\n")
         output_file.write("-------------------------------------------------------------------------------------------------------\n")
         for line in jobstat:
             #search for lines with the string " RU " for running jobs
@@ -36,12 +39,12 @@ with open("/opt/cascripts/shell/longrun_a.dat","r") as jobstat:
                     duration = currentDate - date
                     longrun_threshold = timedelta(days=4)
                     if duration > longrun_threshold:
-                        #remove microseconds from the duration
+                        #convert the duration to string then remove microseconds
                         duration = str(duration).split(".")[0]
                         line = line.replace(string1_to_replace, duration)
                         line = line.replace(string2_to_replace, " ")
-                        #search for regex pattern of the run number that ends with "/1" and remove them
-                        pattern = re.sub(r"[0-9]+\/\d", "", line)
+                        #search for pattern of the run number that ends with "/1" and remove them
+                        pattern = re.sub(r"\d{7}/\d", "", line)
                         # write the lines without the run number pattern
                         output_file.write(pattern)
                 #count running, on hold and on ice jobs, included in "if running jobs in line" loop
@@ -53,43 +56,22 @@ with open("/opt/cascripts/shell/longrun_a.dat","r") as jobstat:
             if on_ice_jobs in line:
                 oi_job_count+=1
 
+#exclude unwanted jobs this is a temporary solution for the final output
+with open(CompleteFilePath, "r") as outfile2:
+    lines = outfile2.readlines()
+
+with open(CompleteFilePath, "w") as outfile2:
+    for line in lines:
+        if not line.startswith('NAT_P_'):
+            outfile2.write(line)
+
 #append the Total Counts
-with open("runjob_output.tsv", "a+") as outfile2:
-    outfile2.seek(0)
-    outfile2.write("\n")
+with open(CompleteFilePath, "a+") as outfile3:
+    outfile3.seek(0)
+    outfile3.write("\n")
     #convert the total running , on hold and on ice jobs to a string
-    outfile2.write("Total Running Jobs: " + str(ru_job_count))
-    outfile2.write("\n")
-    outfile2.write("Total On Hold Jobs: " + str(oh_job_count))
-    outfile2.write("\n")
-    outfile2.write("Total On Ice Jobs: " + str(oi_job_count))
-
-
-print(f"Total Running Jobs: ", ru_job_count)
-print(f"Total On Hold Jobs: ", oh_job_count)
-print(f"Total On Ice Jobs: ", oi_job_count)
-
-
-# open the runjob_output.txt as readable
-#with open("runjob_output.txt", "r") as infile:
-  #  data = infile.read()
-# date finder finds datetime in the text file then computes for the duration of a job run and convert it to string
-      #  string_to_replace = "-----"
-      #  currDT = datetime.now()
-      #  interval = "default"
-      #  duration = currDT - date
-      #  duration = str(duration).split(".")[0]
-       # print('Run Duration: ', duration)
-
-#with open ("runjob_output.txt", "r") as  
-
-#with open('runjob_output.txt', 'w') as outfile:
- #   outfile.write(data)
-#print("Text Replaced")
-
-# Function to convert datetime object to string format
-#def convert(dt_obj):
-#    format = '%m/%d/%y %H:%M:%S'  # The format
-#    datetime_str = datetime.strftime(dt_obj, format)
-#    return datetime_str
-
+    outfile3.write("Total Running Jobs: " + str(ru_job_count))
+    outfile3.write("\n")
+    outfile3.write("Total On Hold Jobs: " + str(oh_job_count))
+    outfile3.write("\n")
+    outfile3.write("Total On Ice Jobs: " + str(oi_job_count))
